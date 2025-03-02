fdc6f9bf2268d86f6b55180df4af419ca24e183c
Wemppy4 <w3mppy@gmail.com>
2025-03-02 17:27:47 +0100

    added smooth mouth rig

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ python open_audio.py
 
 if %errorlevel% equ 0 (
     echo open_audio.py ended successfully. main.py will be started.
-    python main.py
+    python lip_sync.py
 ) else (
     echo open_audio.py ended with an error. main.py will not be started.
     pause
---
+++ start_smooth.bat
@@ -0,0 +1,10 @@
+@echo off
+python open_audio.py
+
+if %errorlevel% equ 0 (
+    echo open_audio.py ended successfully. main.py will be started.
+    python lip_sync_smooth.py
+) else (
+    echo open_audio.py ended with an error. main.py will not be started.
+    pause
+)
